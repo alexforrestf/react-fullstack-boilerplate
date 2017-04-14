4611526ea3b5954f9caf93fd25e2a78257bd57d9
Alex Forrest <alexforrestf2002@yahoo.co.uk>
2017-04-14 11:45:40 +0100

    Initial checkin

---
+++ src/main/java/com/alexanderforrest/ReactFullstackBoilerplateApplication.java
@@ -0,0 +1,12 @@
+package com.alexanderforrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactFullstackBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactFullstackBoilerplateApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alexanderforrest/ReactFullstackBoilerplateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alexanderforrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactFullstackBoilerplateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
